8053c0e736a8be88c8bb852d092c09c5fc899224
Progna99 <progna.chowdhury99@gmail.com>
2021-06-17 21:10:49 +0530

    shop api updated

--- Routes/shop/shop.js
+++ Routes/shop/shop.js
@@ -1,10 +1,12 @@
 const app=require("express");
 const shop = app.Router();
+const mongoose=require("mongoose");
+const shopSchema= require("../../dbmodel/shop");
 
 //New Shop Register Request
     shop.post('/shopsignin',(req,res,next)=>{
 
-        const newShop = new customerSchema({
+        const newShop = new shopSchema({
                 name : req.body.name,
                 address: req.body.address,
                 phonenumber: req.body.phonenumber,
@@ -16,5 +18,23 @@ const shop = app.Router();
         res.status(200).send(newShop);
         next();
 });
+
+customer.get("/shoplogin/:userid",(req,res,next)=>{
+
+        shopSchema.find({"username":userid}).then(result =>{
+                console.log(result.password);
+        res.status(200).send(result.password);        
+        });       
+        next();
+    });
+    
+    // Fetch All shop data for testing
+    customer.get("/allshop",(req,res,next)=>{
+        shopSchema.find({}).then( result =>{
+            console.log(result);
+            res.status(200).send(result); next();
+        }).catch((err)=>{console.log(err)});
+    });
+
 
 module.exports=shop;
